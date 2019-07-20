f61ab444742fb724dbaa71b326cc24daf6876e5d
MrExplode <pjanos@outlook.hu>
2019-07-20 14:13:01 +0200

    base

---
+++ src/main/java/me/mrexplode/notifications/EventListener.java
@@ -0,0 +1,21 @@
+package me.mrexplode.notifications;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+
+public class EventListener implements Listener {
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        
+    }
+    
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        
+    }
+
+}
---
+++ src/main/java/me/mrexplode/notifications/Main.java
@@ -0,0 +1,17 @@
+package me.mrexplode.notifications;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new EventListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+    
+}
